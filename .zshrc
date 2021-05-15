8743c2007c9faf6ade768c718d570f71e3636a96
Vasiliy Yakliushin <vyaklushin@gmail.com>
2021-05-15 20:52:17 +0200

    Remove old configurations

--- .zshrc
+++ .zshrc
@@ -6,15 +6,8 @@ bindkey -e
 
 export PATH="/usr/local/git/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:$PATH"
 
-# Rbenv
-eval "$(rbenv init -)"
-
 autoload -U select-word-style
 select-word-style bash
-
-zstyle :compinstall filename '/home/java/.zshrc'
-
-export PYTHONDOCS=/usr/share/doc/python/html/
 
 PROMPT=$'%{\e[1;32m%}%T %{\e[0m%}%n %{\e[0;36m%}[%{\e[1;36m%}%c%{\e[0;36m%}]%{\e[1;35m%}%#%{\e[0m%} '
 
--- magic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-aptitude install alsa-utils apache2 bzip2 cowsay diff dosfstools emacs23 eog evince feh iceweasel iceweasel-l10n-ru iceweasel-firebug freej git-core ipython openjdk-6-jdk ark links2 mplayer mysql-server htop openbox openssh-server p7zip php5 pv samba scrot sdcv siege rtorrent tcpdump thunar tint2 ttf-dejavu unzip vlc wine x11vnc xclip sudo xorg
-
-git clone https://github.com/javadot/workspace.git
-cd workspace
